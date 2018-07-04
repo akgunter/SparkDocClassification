183b7171f71d9d0772e2bd72ed99b07737e970fd
Alex Gunter <akgunter@go.olemiss.edu>
2018-07-04 09:57:55 -0500

    Refactored pipeline and added printouts

--- src/main/scala/net/ddns/akgunter/scala_classifier/document_classifier/RunClassifier.scala
+++ src/main/scala/net/ddns/akgunter/scala_classifier/document_classifier/RunClassifier.scala
@@ -2,10 +2,10 @@ package net.ddns.akgunter.scala_classifier.document_classifier
 
 import java.nio.file.Paths
 
-import org.apache.spark.ml.feature.{IDF, PCA}
 import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.{ChiSqSelector, PCA}
+import org.apache.spark.ml.feature.{ChiSqSelector, IDF, PCA}
+import org.apache.spark.ml.linalg.SparseVector
 import org.apache.spark.ml.Pipeline
 
 import scala.util.Random
@@ -109,27 +109,39 @@ object RunClassifier extends CanSpark {
       .setLabelCol("label")
       .setOutputCol("chi_sel_features")
       .setSelectorType("fpr")
-      .setFpr(0.15)
+      .setFpr(0.05)
     val pca = new PCA()
       .setInputCol("chi_sel_features")
       .setK(100)
       .setOutputCol("pca_features")
-    val mlpc = new MultilayerPerceptronClassifier()
-      .setLayers(Array(pca.getK, numClasses))
-      .setMaxIter(100)
-      //.setBlockSize(20)
-      .setFeaturesCol("pca_features")
 
-    val pipeline = new Pipeline()
-        .setStages(Array(wordVectorizer, idf, chiSel, pca, mlpc))
+    val preprocPipeline = new Pipeline()
+        .setStages(Array(wordVectorizer, idf, chiSel))
 
     logger.info("Loading data...")
     val trainingData = dataFrameFromDirectory(trainingDir, training = true)
     val validationData = dataFrameFromDirectory(validationDir, training = true)
     //val testingData = dataFrameFromDirectory(testingDir, training = false)
 
-    logger.info("Fitting pipeline...")
-    val model = pipeline.fit(trainingData)
+    logger.info("Fitting preprocessing pipeline...")
+    val dataModel = preprocPipeline.fit(trainingData)
+
+    logger.info("Preprocessing data...")
+    val trainingDataProcessed = dataModel.transform(trainingData)
+    val validationDataProcessed = dataModel.transform(validationData)
+
+    logger.info("Configuring neural network...")
+    trainingDataProcessed.printSchema()
+    val numFeatures = trainingDataProcessed.head.getAs[SparseVector]("chi_sel_features").size
+
+    val mlpc = new MultilayerPerceptronClassifier()
+      .setLayers(Array(numFeatures, numClasses))
+      .setMaxIter(100)
+      //.setBlockSize(20)
+      .setFeaturesCol("pca_features")
+
+    /*
+    logger.info("Training neural network...")
 
     logger.info("Calculating predictions...")
     val trainingPredictions = model.transform(trainingData)
@@ -141,6 +153,7 @@ object RunClassifier extends CanSpark {
 
     logger.info(s"Training accuracy: ${evaluator.evaluate(trainingPredictions)}")
     logger.info(s"Validation accuracy: ${evaluator.evaluate(validationPredictions)}")
+    */
   }
 
 
