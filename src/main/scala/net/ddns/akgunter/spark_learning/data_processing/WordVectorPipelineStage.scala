c05d1dba167e077a536e3bf09927d50f89b48526
Alex Gunter <akgunter@go.olemiss.edu>
2018-07-05 17:40:58 -0500

    Corrected schema construction and added debug printout

--- src/main/scala/net/ddns/akgunter/spark_learning/data_processing/WordVectorPipelineStage.scala
+++ src/main/scala/net/ddns/akgunter/spark_learning/data_processing/WordVectorPipelineStage.scala
@@ -21,8 +21,11 @@ trait WordVectorPipelineStage extends PipelineStage with WordVectorParams {
     val requiredColumnTypes = WordVectorParams.COLUMN_TYPES.filter {
       case (colStr, _) => requiredColumnStrings(colStr)
     }
+    val inputColumnTypes = schema.fieldNames.map {
+      colStr => colStr -> schema.fields(schema.fieldIndex(colStr)).dataType
+    }.toMap
     val failedColTypes = requiredColumnTypes.map {
-      case (colStr, reqColType) => colStr -> (reqColType, schema.fields(schema.fieldIndex(colStr)).dataType)
+      case (colStr, reqColType) => colStr -> (reqColType, inputColumnTypes(colStr))
     }.filterNot {
       case (_, (reqColType, realColType)) => reqColType == realColType
     }
@@ -34,10 +37,10 @@ trait WordVectorPipelineStage extends PipelineStage with WordVectorParams {
       }.mkString(", ")}"
     )
 
-    val outSchema = WordVectorPipelineStage.buildSchema(requiredColumnTypes)
-    outSchema.printTreeString()
+    val outputColumnTypes = inputColumnTypes ++ requiredColumnTypes
+    val outSchema = WordVectorPipelineStage.buildSchema(outputColumnTypes)
 
-    if (schema.fieldNames.contains($(labelCol)))
+    if (schema.fieldNames.contains($(labelCol)) && !outSchema.fieldNames.contains($(labelCol)))
       outSchema.add($(labelCol), IntegerType)
     else
       outSchema
@@ -45,12 +48,14 @@ trait WordVectorPipelineStage extends PipelineStage with WordVectorParams {
 }
 
 object WordVectorPipelineStage {
-  protected def buildSchema(requiredColumnTypes: Map[String, DataType]): StructType = {
-    buildSchema(requiredColumnTypes.iterator, new StructType())
+  protected def buildSchema(columnTypes: Map[String, DataType]): StructType = {
+    println(columnTypes.mkString(", "))
+    buildSchema(columnTypes.iterator, new StructType())
   }
 
   protected def buildSchema(colTypeIter: Iterator[(String, DataType)], schema: StructType): StructType = {
-    if (!colTypeIter.hasNext) schema
+    if (!colTypeIter.hasNext)
+      schema
     else {
       val (nextCol, nextType) = colTypeIter.next
       buildSchema(colTypeIter, schema.add(nextCol, nextType))
