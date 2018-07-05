0a12c223822e76d10932586785e07158cb77cb8f
Alex Gunter <akgunter@go.olemiss.edu>
2018-07-05 18:29:15 -0500

    Dropped FPR to 0.02

--- src/main/scala/net/ddns/akgunter/spark_learning/document_classifier/RunClassifier.scala
+++ src/main/scala/net/ddns/akgunter/spark_learning/document_classifier/RunClassifier.scala
@@ -34,7 +34,7 @@ object RunClassifier extends CanSpark {
       .setLabelCol("label")
       .setOutputCol("chi_sel_features")
       .setSelectorType("fpr")
-      .setFpr(0.05)
+      .setFpr(0.02)
     val pca = new PCA()
       .setInputCol("chi_sel_features")
       .setK(8000)
