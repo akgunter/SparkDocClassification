44ed3892bc3dacddee8b6895f6e00af20e2d1799
Alexander Gunter <akgunter@go.olemiss.edu>
2018-07-10 15:30:59 -0500

    Tweaked training master config

--- src/main/scala/net/ddns/akgunter/spark_learning/document_classifier/RunClassifier.scala
+++ src/main/scala/net/ddns/akgunter/spark_learning/document_classifier/RunClassifier.scala
@@ -147,10 +147,10 @@ object RunClassifier extends CanSpark {
       .controllerAddress("127.0.0.1")
       .build
 
-    val tm = new SharedTrainingMaster.Builder(voidConfig, 16)
+    val tm = new SharedTrainingMaster.Builder(voidConfig, 1)
       .updatesThreshold(1e-3)
       .rddTrainingApproach(RDDTrainingApproach.Direct)
-      .batchSizePerWorker(16)
+      .batchSizePerWorker(1)
       .workersPerNode(4)
       .build
 
